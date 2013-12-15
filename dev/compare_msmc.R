b38837aefda34b99b93baa7b0929a26e76987d20
mathii <mathii@mathiis-MacBook-Air.local>
2013-12-15 18:49:11 -0500

    merging

--- dev/compare_msmc.R
+++ dev/compare_msmc.R
@@ -1,13 +1,16 @@
 ## compare msmsc gene flow estimates to the between population
 ## f2 density estimates.
 
+code.dir <- "~/f2_age/code/"
+source("~/f2_age/code/libs/include.R")
+
 ## sim.type="ancient_split_long_migration" 
-## sim.type="ancient_split_migration" 
+sim.type="ancient_split_migration" 
 ## sim.type="ancient_split" 
 ## sim.type="third_party" 
-sim.type="ancient_split_migration_growth" 
+## sim.type="ancient_split_migration_growth" 
 ## sim.type="ancient_split_migration_bottleneck" 
-## sim.type="## ancient_split_migration_200" 
+## sim.type="ancient_split_migration_200" 
 
 chr="20"
 
@@ -16,29 +19,34 @@ dyn.load("~/f2_age/code/libs/inference.so")
 msmc <- read.table(paste0("~/f2_age/simulations/", sim.type, "/chr", chr, "/results/msmc_results_all.final.txt"), header=T, as.is=T)
 
 within <- (matched$ID1<51 & matched$ID2<51)|(matched$ID1>50 & matched$ID2>50)
-## within <- (matched$ID1<101 & matched$ID2<101)|(matched$ID1>100 & matched$ID2>100)
+if(grepl("200", sim.type)){within <- (matched$ID1<101 & matched$ID2<101)|(matched$ID1>100 & matched$ID2>100)}
 between <- !within
 
-alpha.w <- round(0.05*sum(within))
-alpha.b <- round(0.05*sum(between))
-alpha <- round(0.05*NROW(matched))
+## alpha.w <- round(0.05*sum(within))
+## alpha.b <- round(0.05*sum(between))
+## alpha <- round(0.05*NROW(matched))
+## ## d.b <- estimate.t.density.mcmc(0*matched$map.len[between,] ,0*matched$f2[between,], Ne, p.fun, verbose=FALSE, logt.grid=logt.grid, prior=norm.2.p, alpha=alpha,error.params=NA, n.sims=10000, thin=100, ll.mat=ll.mats[[6]][between,])
 
-d.b <- estimate.t.density.mcmc(0*matched$map.len[between,] ,0*matched$f2[between,], Ne, p.fun, verbose=FALSE, logt.grid=logt.grid, prior=norm.2.p, alpha=alpha,error.params=NA, n.sims=10000, thin=100, ll.mat=ll.mats[[6]][between,])
-t.hats<-MLE.from.haps(matched, Ne, S.params=S.params, error.params=error.params, verbose=TRUE)
+t.hats<-MLE.from.haps(matched[between,], Ne, S.params=S.params[between,], error.params=error.params, verbose=TRUE)
+t.hat.dens <- density(log10(t.hats))
 
-## par(mar=c(5.1,4.1,2.1,4.1))
-## plot(msmc$left_time_boundary/1.2e-8, 2*msmc$lambda_01/(msmc$lambda_00+msmc$lambda_11), type="s", log="x", ylim=c(0,1), xlim=c(200,2000), bty="n", xlab="Generations", ylab="MSMC gene flow estimate", col="#E41A1C", yaxt="n", main=sim.type)
-## axis(2, col="#E41A1C")
-## scale <- max(d.b(logt.grid))
-## lines(10^logt.grid, d.b(logt.grid)/scale, col="#377EBA")
-## axis(4, col="#377EBA", labels=FALSE)
-## mtext(expression(f[2]~age~density), 4, line=2)
-## abline(v=1120, lty=3)
-## abline(v=560, lty=3)
+par(mar=c(5.1,4.1,2.1,4.1))
+plot(msmc$left_time_boundary/1.2e-8, 2*msmc$lambda_01/(msmc$lambda_00+msmc$lambda_11), type="s", log="x", ylim=c(0,1), xlim=c(100,10000), bty="n", xlab="Generations", ylab="MSMC gene flow estimate", col="#E41A1C", yaxt="n", main=sim.type)
+axis(2, col="#E41A1C")
+scale <- max(t.hat.dens$y)
+lines(10^t.hat.dens$x, t.hat.dens$y/scale, col="#377EBA")
+abline(v=median(t.hats), col="#377EBA", lty=2)
+axis(4, col="#377EBA", labels=FALSE)
+mtext(expression(f[2]~age~density), 4, line=2)
+abline(v=1120, lty=3)
+if(grepl("migration", sim.type)){abline(v=560, lty=3)}
 
-par(mfrow=c(2,2))
-hist(log10(matched$Age[between]), breaks=100, xlim=c(0,5))
-hist(log10(t.hats[between]), breaks=100, xlim=c(0,5))
-hist(log10(matched$Age[within]), breaks=100, xlim=c(0,5))
-hist(log10(t.hats[within]), breaks=100, xlim=c(0,5))
+## scale2 <- max(d.b(logt.grid))
+## lines(logt.grid, d.b(logt.grid)/scale2, col="#377EBA", lty=2)
+
+## par(mfrow=c(2,2))
+## hist(log10(matched$Age[between]), breaks=100, xlim=c(0,5))
+## hist(log10(t.hats[between]), breaks=100, xlim=c(0,5))
+## hist(log10(matched$Age[within]), breaks=100, xlim=c(0,5))
+## hist(log10(t.hats[within]), breaks=100, xlim=c(0,5))
 
--- dev/compare_msmc_CEU_CHB.R
+++ dev/compare_msmc_CEU_CHB.R
@@ -4,6 +4,7 @@
 sim.type="CEU_CHB" 
 
 msmc <- read.table("~/f2_age/1000g/CHB_CEU_4combined_0,1,4,5_msmc.final.txt", header=T, as.is=T)
+msmc$flow <- 2*msmc$lambda_01/(msmc$lambda_00+msmc$lambda_11)
 
 ######################################################################################################
 
@@ -17,9 +18,8 @@ chrs <- c(1:22)
 
 ######################################################################################################
 
-if(! "d.b" %in% ls() ){
-  haps <- list()
-  ll.mat <- list()
+if(TRUE ){
+  t.hats <- list()
   subenv <- new.env()
   logt.grid=NULL
 
@@ -30,47 +30,33 @@ if(! "d.b" %in% ls() ){
     cat(paste("\r", chr))
     load(paste(chr.res.dir, "/chr", chr, "/results/ll_environment.Rdata", sep=""), envir=subenv)
     ll.mat[[i]] <- subenv$ll.mat
+
+    haps <- subenv$haps
+
+    ID1.pop <- pop.map[haps$ID1]
+    ID2.pop <- pop.map[haps$ID2]
+    include <- (ID1.pop=="CEU"&ID2.pop=="CHB")|(ID2.pop=="CEU"&ID1.pop=="CHB")
     
-    haps[[i]] <- subenv$haps
+    t.hats[[i]]<-MLE.from.haps(haps[include,], subenv$Ne, S.params=subenv$S.params[include,], error.params=subenv$error.params, verbose=TRUE)
     
-    if(NROW(ll.mat[[i]])!=NROW(haps[[i]])){stop(paste0("Error in chr", chr))}
-  
-    if(i==1){
-      logt.grid <- subenv$logt.grid
-    }else if (!all(subenv$logt.grid==logt.grid)){
-      stop("grids do not match")
-    }
   i=i+1
   }
   cat("\n")
   
-  ll.mat <- do.call("rbind", ll.mat)
-  haps <- do.call("rbind", haps)
-  
-  ## the following is cnp'd from run_1kg_analysis_chr.R.
-  ## estimate densities, by population.
-  populations <- sort(unique(pop.map))
-  npop <- length(populations)             #14
-  densities <- rep(list(list()),npop)
+  t.hats <- do.call("c", t.hats)
+  t.hat.dens <- density(log10(t.hats))
 
-  ID1.pop <- pop.map[haps$ID1]
-  ID2.pop <- pop.map[haps$ID2]
-  
-  include <- (ID1.pop=="CEU"&ID2.pop=="CHB")|(ID2.pop=="CEU"&ID1.pop=="CHB")
-  
-  alpha <- round(0.05*sum(include))
-  d.b <- estimate.t.density.mcmc(0 ,0, Ne, p.fun, verbose=FALSE, logt.grid=logt.grid, prior=norm.2.p, alpha=alpha,error.params=NA, n.sims=10000, thin=100, ll.mat=ll.mat[include,])
 }
 
 par(mar=c(5.1,4.1,2.1,4.1))
-plot(0.03*msmc$left_time_boundary/1.25e-8, 2*msmc$lambda_01/(msmc$lambda_00+msmc$lambda_11), type="s", log="x", ylim=c(0,1), bty="n", xlab="Time (kya)", ylab="MSMC gene flow estimate", col="#E41A1C", yaxt="n", lwd=2, xaxt="n")
+plot(0.03*msmc$left_time_boundary/1.25e-8, 2*msmc$lambda_01/(msmc$lambda_00+msmc$lambda_11), type="s", log="x", ylim=c(0,1), bty="n", xlab="Time (kya)", ylab="MSMC gene flow estimate", col="#E41A1C", yaxt="n", lwd=2, xaxt="n", xlim=c(1,500))
 axis(2, col="#E41A1C")
-axis(1, at=c(seq(5,10,1), seq(20,100,10), seq(200,500,100)), labels=FALSE)
-mtext(c(5,10,20,50,100,200,500), 1,at= c(5,10,20,50,100,200,500), line=1)
-scale <- max(d.b(logt.grid))
-lines(0.03*10^logt.grid, d.b(logt.grid)/scale, col="#377EBA", lwd=2)
+axis(1, at=c(seq(1,10,1), seq(20,100,10), seq(200,500,100)), labels=FALSE)
+mtext(c(1,2,5,10,20,50,100,200,500), 1,at= c(1,2,5,10,20,50,100,200,500), line=1)
+scale <- max(t.hat.dens$y)
+lines(0.03*10^t.hat.dens$x, t.hat.dens$y/scale, col="#377EBA", lwd=2)
 axis(4, col="#377EBA", labels=FALSE, lwd=2, at=c(0,1))
 mtext(expression(f[2]~age~density), 4, line=2)
-abline(v=40, lty=3, col="#E41A1C")
-abline(v=11, lty=3, col="#377EBA")
+abline(v=0.03*msmc$left_time_boundary[max(which(msmc$flow<0.5))+1]/1.25e-8, lty=3, col="#E41A1C")
+abline(v=median(0.03*t.hats), lty=3, col="#377EBA")
 
