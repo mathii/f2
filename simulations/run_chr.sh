2904ea0403a874e3732dc296152254cb323aa2c7
Iain Mathieson <mathii@well.ox.ac.uk>
2013-12-19 17:52:13 -0500

    fix

--- simulations/run_chr.sh
+++ simulations/run_chr.sh
@@ -13,7 +13,7 @@ sim_type=$3
 run_sim_type=$3
 wrong_map=""
 
-if [! -z $4]; then
+if [ -n $4]; then
     wrong_map=$4
     sim_type=${sim_type}_${wrong_map}
 fi
@@ -27,7 +27,7 @@ MACS_DIR=~/Packages/macs
 # Where do you want the simulations to go?
 SIMS_DIR=/data1/users/mathii/f2_sims/${sim_type}/chr${CHR}
 # Where is the recombination map, in impute format?
-HM2_MAP=~/recombination_map2/hm2/genetic_map_GRCh37_chr${CHR}.txt.gz
+HM2_MAP=~/recombination_maps/hm2/genetic_map_GRCh37_chr${CHR}.txt.gz
 # Where is the code - this point to the directory you downloaded from github
 CODE_DIR=~/f2/f2_age
 
@@ -53,8 +53,6 @@ do
     mkdir -p ${dir}
 done
 
-sleep ${sleep_time}
-
 # redirect output to logfile                                                                                                                                   
 LOG=${RD}/log.txt
 # exec > ${LOG} 2>&1
@@ -67,7 +65,7 @@ rho=`echo "4*$ne*0.00000001" | bc`
 R --vanilla --args ${HM2_MAP} ${MD}/map.txt ${MD}/cut.map.txt < ${CD}/scripts/convert_HM_maps_to_macs_format.R
 
 # 1a) If we are trying to simulate with the wrong map
-if [! -z ${wrong_map}]; then
+if [ -n ${wrong_map}]; then
     case ${wrong_map} in
 	rm  ${MD}/map.txt
 	aa_map)
