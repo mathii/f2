28323d76f6180dd51a16052ada01d560e224da3c
mathii <mathii@mathiis-MacBook-Air.local>
2014-01-22 01:20:03 -0800

    rm old file

--- analysis/run_all_fn.sh
+++ analysis/run_all_fn.sh
@@ -5,6 +5,7 @@ CODE_DIR=~/f2/f2_age
 SEQ_DATA=/data1/users/mathii/1000g/data/seq/ALL.chrXX.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz
 N_PROCS=22
 
+rm ${SIMS_ROOT}/tmp_args_fn
 for chr in {1..22}
 do
     seq_path=${SEQ_DATA/XX/$chr}
